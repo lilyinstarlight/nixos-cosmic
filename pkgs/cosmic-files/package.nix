3beb254f27d30e007b376f18314a7693cc6f643a
Lily Foster <lily@lily.flowers>
2024-02-28 18:12:17 -0500

    cosmic-files: 0-unstable-2024-02-26 -> 0-unstable-2024-02-28
    Diff: https://github.com/pop-os/cosmic-files/compare/5778d64e8cc8540221459e4b3c463da9bf2ee60b...dc8433ce590995fce52572f3f2521ac469e0126e

--- pkgs/cosmic-files/package.nix
+++ pkgs/cosmic-files/package.nix
@@ -6,15 +6,15 @@
 , stdenv
 }:
 
-rustPlatform.buildRustPackage {
+rustPlatform.buildRustPackage rec {
   pname = "cosmic-files";
-  version = "0-unstable-2024-02-26";
+  version = "0-unstable-2024-02-28";
 
   src = fetchFromGitHub {
     owner = "pop-os";
     repo = "cosmic-files";
-    rev = "5778d64e8cc8540221459e4b3c463da9bf2ee60b";
-    hash = "sha256-mQ5YJXVFTPfzAf6Ugzb16IlJ7pkFjjQqJVXu/Us/XGc=";
+    rev = "dc8433ce590995fce52572f3f2521ac469e0126e";
+    hash = "sha256-gz+J48S+A7TSc8p5dNx61j0RCt11tsuL+rpw4uR2ec8=";
   };
 
   cargoLock = {
@@ -22,7 +22,7 @@ rustPlatform.buildRustPackage {
     outputHashes = {
       "accesskit-0.12.2" = "sha256-ksaYMGT/oug7isQY8/1WD97XDUsX2ShBdabUzxWffYw=";
       "atomicwrites-0.4.2" = "sha256-QZSuGPrJXh+svMeFWqAXoqZQxLq/WfIiamqvjJNVhxA=";
-      "cosmic-config-0.1.0" = "sha256-lFX/x4Abfk+ZPeFXY02LweMj6bEn+WfGa5e8SAUZGcc=";
+      "cosmic-config-0.1.0" = "sha256-eaG/HCwlKqSfEp6GEPeBS63j5WHq4qdYTNHqnW2zeeE=";
       "cosmic-text-0.11.2" = "sha256-Y9i5stMYpx+iqn4y5DJm1O1+3UIGp0/fSsnNq3Zloug=";
       "d3d12-0.19.0" = "sha256-usrxQXWLGJDjmIdw1LBXtBvX+CchZDvE8fHC0LjvhD4=";
       "glyphon-0.5.0" = "sha256-j1HrbEpUBqazWqNfJhpyjWuxYAxkvbXzRKeSouUoPWg=";
@@ -45,6 +45,8 @@ rustPlatform.buildRustPackage {
     "bin-src"
     "target/${stdenv.hostPlatform.rust.cargoShortTarget}/release/cosmic-files"
   ];
+
+  env.VERGEN_GIT_SHA = src.rev;
 
   meta = with lib; {
     homepage = "https://github.com/pop-os/cosmic-files";
