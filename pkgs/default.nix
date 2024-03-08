9cdfe24715bf8d1a6c3c62c3a57339d92cc6198f
Lily Foster <lily@lily.flowers>
2024-03-08 18:40:37 -0500

    pkgs: emulator buildPackages and targetPackages

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,6 +3,6 @@
 let
   dir = builtins.readDir ./.;
   pkgsPaths = builtins.filter (pkgPath: pkgPath != null) (builtins.map (name: if dir.${name} == "directory" then { inherit name; value = "${builtins.toString ./.}/${name}/package.nix"; } else null) (builtins.attrNames dir));
-  callPackage = if final != null then final.callPackage else pkgs.lib.callPackageWith (pkgs // finalPkgs // { inherit callPackage; });
+  callPackage = if final != null then final.callPackage else pkgs.lib.callPackageWith (pkgs // finalPkgs // { buildPackages = finalPkgs; targetPackages = finalPkgs; inherit callPackage; });
   finalPkgs = builtins.listToAttrs (builtins.map (pkgPath: { inherit (pkgPath) name; value = callPackage pkgPath.value {}; }) pkgsPaths);
 in finalPkgs
