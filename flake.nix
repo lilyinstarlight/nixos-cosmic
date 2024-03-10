d9f0cdbc53b3a602e9faea5b2efbe7804639cb3c
Lily Foster <lily@lily.flowers>
2024-03-09 22:57:39 -0500

    flake: add firefox to vm

--- flake.nix
+++ flake.nix
@@ -70,7 +70,7 @@
               services.xserver.desktopManager.cosmic.enable = true;
               services.xserver.displayManager.cosmic-greeter.enable = true;
 
-              environment.systemPackages = [ pkgs.drm_info ];
+              environment.systemPackages = [ pkgs.drm_info pkgs.firefox ];
 
               boot.kernelParams = [ "quiet" "udev.log_level=3"  ];
               boot.initrd.kernelModules = [ "bochs" ];
