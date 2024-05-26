04d6f76a49bff939ce65dcba2c8443e8337cd126
Lily Foster <lily@lily.flowers>
2024-05-26 11:51:54 -0400

    cosmic-comp: 0-unstable-2024-05-22 -> 0-unstable-2024-05-24
    Diff: https://github.com/pop-os/cosmic-comp/compare/ea6b45a5449d1c33c7deb7c61bb519d161d2d89d...4b9cc43924725384cdaeba0c9c88d8ef3904095b

--- pkgs/cosmic-comp/package.nix
+++ pkgs/cosmic-comp/package.nix
@@ -17,13 +17,13 @@
 
 rustPlatform.buildRustPackage {
   pname = "cosmic-comp";
-  version = "0-unstable-2024-05-22";
+  version = "0-unstable-2024-05-24";
 
   src = fetchFromGitHub {
     owner = "pop-os";
     repo = "cosmic-comp";
-    rev = "ea6b45a5449d1c33c7deb7c61bb519d161d2d89d";
-    hash = "sha256-q2lXlPhbEKsNTQ07urkI6EYTgQjgbmevm7OHrVWotUQ=";
+    rev = "4b9cc43924725384cdaeba0c9c88d8ef3904095b";
+    hash = "sha256-kqHVLemkagYZkK1QgkvfPVLXAOlL10Z/PMvetY2HDtE=";
   };
 
   cargoLock = {
@@ -37,7 +37,7 @@ rustPlatform.buildRustPackage {
       "d3d12-0.19.0" = "sha256-usrxQXWLGJDjmIdw1LBXtBvX+CchZDvE8fHC0LjvhD4=";
       "glyphon-0.5.0" = "sha256-j1HrbEpUBqazWqNfJhpyjWuxYAxkvbXzRKeSouUoPWg=";
       "id_tree-1.8.0" = "sha256-uKdKHRfPGt3vagOjhnri3aYY5ar7O3rp2/ivTfM2jT0=";
-      "smithay-0.3.0" = "sha256-YsAULLcuIdY1bdKZ1NtHyYlRFC4l5cpCmYa/OiGJUlc=";
+      "smithay-0.3.0" = "sha256-UAz1FAt6erYpPIb8TCzX6eo2CbUkWgtCkf//pDYxdHo=";
       "smithay-clipboard-0.8.0" = "sha256-OZOGbdzkgRIeDFrAENXE7g62eQTs60Je6lYVr0WudlE=";
       "smithay-egui-0.1.0" = "sha256-FcSoKCwYk3okwQURiQlDUcfk9m/Ne6pSblGAzHDaVHg=";
       "softbuffer-0.4.1" = "sha256-a0bUFz6O8CWRweNt/OxTvflnPYwO5nm6vsyc/WcXyNg=";
