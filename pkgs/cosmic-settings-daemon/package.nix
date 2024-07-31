d2989d7754fdcf845285330e6469f62ca4ed0faf
Lily Foster <lily@lily.flowers>
2024-07-31 15:50:33 -0400

    cosmic-settings-daemon: install data files

--- pkgs/cosmic-settings-daemon/package.nix
+++ pkgs/cosmic-settings-daemon/package.nix
@@ -41,6 +41,12 @@ rustPlatform.buildRustPackage {
 
   env.GEOCLUE_AGENT = "${lib.getLib geoclue2-with-demo-agent}/libexec/geoclue-2.0/demos/agent";
 
+  postInstall = ''
+    mkdir -p $out/share/{polkit-1/rules.d,cosmic/com.system76.CosmicSettings.Shortcuts/v1}
+    cp data/polkit-1/rules.d/*.rules $out/share/polkit-1/rules.d/
+    cp data/system_actions.ron $out/share/cosmic/com.system76.CosmicSettings.Shortcuts/v1/system_actions
+  '';
+
   meta = with lib; {
     homepage = "https://github.com/pop-os/cosmic-settings-daemon";
     description = "Settings Daemon for the COSMIC Desktop Environment";
