56bc2f344bab56e26227f32546c832b0dd715338
Lily Foster <lily@lily.flowers>
2024-08-07 12:38:30 -0400

    cosmic-panel: epoch-1.0.0-alpha.1-unstable-2024-08-02 -> 1.0.0-alpha.1-unstable-2024-08-06
    Diff: https://github.com/pop-os/cosmic-panel/compare/47c672066adac6ff77b680b9461c8e1c2667b758...eb4fcd1ba180057bbab2dc10d2335f6cd530af4b

--- pkgs/cosmic-panel/package.nix
+++ pkgs/cosmic-panel/package.nix
@@ -10,13 +10,13 @@
 
 rustPlatform.buildRustPackage {
   pname = "cosmic-panel";
-  version = "epoch-1.0.0-alpha.1-unstable-2024-08-02";
+  version = "1.0.0-alpha.1-unstable-2024-08-06";
 
   src = fetchFromGitHub {
     owner = "pop-os";
     repo = "cosmic-panel";
-    rev = "47c672066adac6ff77b680b9461c8e1c2667b758";
-    sha256 = "sha256-atcVOSIZV/Tjq43438e0PK1nllPktap6il+0TihEwgY=";
+    rev = "eb4fcd1ba180057bbab2dc10d2335f6cd530af4b";
+    sha256 = "sha256-aw5OlsGTIkZStepTCLGsyG8NsiLssEGmDl0G/bZYLRY=";
   };
 
   cargoLock = {
@@ -26,14 +26,14 @@ rustPlatform.buildRustPackage {
       "atomicwrites-0.4.2" = "sha256-QZSuGPrJXh+svMeFWqAXoqZQxLq/WfIiamqvjJNVhxA=";
       "clipboard_macos-0.1.0" = "sha256-cG5vnkiyDlQnbEfV2sPbmBYKv1hd3pjJrymfZb8ziKk=";
       "cosmic-client-toolkit-0.1.0" = "sha256-1XtyEvednEMN4MApxTQid4eed19dEN5ZBDt/XRjuda0=";
-      "cosmic-config-0.1.0" = "sha256-LRP3NU2V1Qzrs+SQh1iYUFAg5BbTPwb9HvWnsO5d51Y=";
-      "cosmic-notifications-util-0.1.0" = "sha256-VdJ6W8+g2492wAr1kDLo2zWZGc01gHp2dvLVtj/xArE=";
+      "cosmic-config-0.1.0" = "sha256-zdhdsGpf+/38WLDL1goKH22FbZx/Ntz4GUedaToGgEo=";
+      "cosmic-notifications-util-0.1.0" = "sha256-tCizZePze94tbJbR91N9rfUhrLFTAMW2oL9ByKOeDAU=";
       "cosmic-text-0.12.1" = "sha256-x0XTxzbmtE2d4XCG/Nuq3DzBpz15BbnjRRlirfNJEiU=";
       "d3d12-0.19.0" = "sha256-usrxQXWLGJDjmIdw1LBXtBvX+CchZDvE8fHC0LjvhD4=";
       "glyphon-0.5.0" = "sha256-j1HrbEpUBqazWqNfJhpyjWuxYAxkvbXzRKeSouUoPWg=";
       "launch-pad-0.1.0" = "sha256-c+uawTQlg5SW8x7DOBG2Idv/AfIaCFNtLQLUz8ifT2I=";
       "smithay-0.3.0" = "sha256-Ir12itaCX1zDb6QzuBh9ve09KGLxxiiiW/sP4bxlx8w=";
-      "smithay-clipboard-0.8.0" = "sha256-pBQZ+UXo9hZ907mfpcZk+a+8pKrIWdczVvPkjT3TS8U=";
+      "smithay-clipboard-0.8.0" = "sha256-4InFXm0ahrqFrtNLeqIuE3yeOpxKZJZx+Bc0yQDtv34=";
       "softbuffer-0.4.1" = "sha256-a0bUFz6O8CWRweNt/OxTvflnPYwO5nm6vsyc/WcXyNg=";
       "taffy-0.3.11" = "sha256-SCx9GEIJjWdoNVyq+RZAGn0N71qraKZxf9ZWhvyzLaI=";
     };
