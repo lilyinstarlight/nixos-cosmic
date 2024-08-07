3a20c0a56387037f2c8b7ab0b253eef638f82786
Lily Foster <lily@lily.flowers>
2024-08-07 12:28:40 -0400

    cosmic-bg: epoch-1.0.0-alpha.1-unstable-2024-07-31 -> 1.0.0-alpha.1-unstable-2024-07-31
    Diff: https://github.com/pop-os/cosmic-bg/compare/e5637fcc83abeb4e9a675b04a9b98ae4a5c186fa...e5637fcc83abeb4e9a675b04a9b98ae4a5c186fa

--- pkgs/cosmic-bg/package.nix
+++ pkgs/cosmic-bg/package.nix
@@ -10,7 +10,7 @@
 
 rustPlatform.buildRustPackage {
   pname = "cosmic-bg";
-  version = "epoch-1.0.0-alpha.1-unstable-2024-07-31";
+  version = "1.0.0-alpha.1-unstable-2024-07-31";
 
   src = fetchFromGitHub {
     owner = "pop-os";
