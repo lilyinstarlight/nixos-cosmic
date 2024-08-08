4033c07b10d603dcf1b00f6ebb0412932b0d44f0
Lily Foster <lily@lily.flowers>
2024-08-08 19:20:43 -0400

    impure: add nixos module impure entrypoint
    Allows using the NixOS module via
    `imports = [ <nixos-cosmic/module.nix> ];` for including in non-flake
    setups.

---
+++ module.nix
@@ -0,0 +1,14 @@
+let
+  self = (import (
+      let
+        lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+      in fetchTarball {
+        url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.${lock.nodes.${lock.root}.inputs.flake-compat}.locked.rev}.tar.gz";
+        sha256 = lock.nodes.flake-compat.locked.narHash;
+      }
+    )
+    { src = ./.; }
+  ).defaultNix;
+in
+
+self.nixosModules.default
