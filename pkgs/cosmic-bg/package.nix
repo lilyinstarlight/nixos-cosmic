a5617e2b50bb17dd26d2ff040f209a1be111975a
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-09-01 16:54:45 +0100

    cosmic-bg: 1.0.0-alpha.1-unstable-2024-08-27 -> 1.0.0-alpha.1-unstable-2024-08-31

--- pkgs/cosmic-bg/package.nix
+++ pkgs/cosmic-bg/package.nix
@@ -10,18 +10,13 @@
 
 rustPlatform.buildRustPackage {
   pname = "cosmic-bg";
-  version = "1.0.0-alpha.1-unstable-2024-08-27";
+  version = "1.0.0-alpha.1-unstable-2024-08-31";
 
   src = fetchFromGitHub {
     owner = "pop-os";
     repo = "cosmic-bg";
-    rev = "e5e91d93fb7cd7e917922eceaddd4d412df46f93";
+    rev = "b327d3720cbd4aad5c308a005ba89e42ad354533";
     hash = "sha256-3hhS4/+5wj5pjVKdS+BPy8Q7UUFaMKiGANfDIRBm+Dc=";
-    # TODO: remove when <https://github.com/pop-os/cosmic-bg/commit/ed9ea6cc15638b08c848fda042ec2df3ff69865f#r145966372> is addressed
-    postFetch = ''
-      substituteInPlace $out/Cargo.toml --replace-fail 'libcosmic?branch=master' '/libcosmic'
-      substituteInPlace $out/Cargo.lock --replace-fail 'libcosmic?branch=master?rev=' '/libcosmic?rev='
-    '';
   };
 
   cargoLock = {
