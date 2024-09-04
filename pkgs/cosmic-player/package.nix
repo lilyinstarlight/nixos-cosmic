52385251ab37c8c098791f9f514be877b11f87ff
Lily Foster <lily@lily.flowers>
2024-09-04 18:06:54 -0400

    cosmic-player: init at 0-unstable-2024-07-02

---
+++ pkgs/cosmic-player/package.nix
@@ -0,0 +1,77 @@
+{ lib
+, fetchFromGitHub
+, rustPlatform
+, libcosmicAppHook
+, alsa-lib
+, ffmpeg
+, just
+, pkg-config
+, stdenv
+, nix-update-script
+}:
+
+rustPlatform.buildRustPackage {
+  pname = "cosmic-player";
+  version = "0-unstable-2024-07-02";
+
+  src = fetchFromGitHub {
+    owner = "pop-os";
+    repo = "cosmic-player";
+    rev = "52b9439ca4ff4d2daeefc18ea5ba90cc8c36886c";
+    hash = "sha256-2q3ZTuH4aS+VDppy8KUgFqf8sHQnM1ILWfnHZ0GTCW4=";
+  };
+
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+    outputHashes = {
+      "accesskit-0.12.2" = "sha256-ksaYMGT/oug7isQY8/1WD97XDUsX2ShBdabUzxWffYw=";
+      "atomicwrites-0.4.2" = "sha256-QZSuGPrJXh+svMeFWqAXoqZQxLq/WfIiamqvjJNVhxA=";
+      "clipboard_macos-0.1.0" = "sha256-temNg+RdvquSLAdkwU5b6dtu9vZkXjnDASS/eJo2rz8=";
+      "cosmic-config-0.1.0" = "sha256-RVUsezjeJ3EGsD+zTWMwxLP+0HvbxFKuuW6JivWfCKE=";
+      "cosmic-text-0.11.2" = "sha256-2TfgUaxyRBrf9EpcdOzrwQltoI9/Od29itrxldZ5KGw=";
+      "d3d12-0.19.0" = "sha256-usrxQXWLGJDjmIdw1LBXtBvX+CchZDvE8fHC0LjvhD4=";
+      "glyphon-0.5.0" = "sha256-j1HrbEpUBqazWqNfJhpyjWuxYAxkvbXzRKeSouUoPWg=";
+      "smithay-client-toolkit-0.18.0" = "sha256-/7twYMt5/LpzxLXAQKTGNnWcfspUkkZsN5hJu7KaANc=";
+      "smithay-clipboard-0.8.0" = "sha256-MqzynFCZvzVg9/Ry/zrbH5R6//erlZV+nmQ2St63Wnc=";
+      "softbuffer-0.4.1" = "sha256-a0bUFz6O8CWRweNt/OxTvflnPYwO5nm6vsyc/WcXyNg=";
+      "taffy-0.3.11" = "sha256-SCx9GEIJjWdoNVyq+RZAGn0N71qraKZxf9ZWhvyzLaI=";
+      "winit-0.29.10" = "sha256-ScTII2AzK3SC8MVeASZ9jhVWsEaGrSQ2BnApTxgfxK4=";
+    };
+  };
+
+  nativeBuildInputs = [
+    libcosmicAppHook
+    rustPlatform.bindgenHook
+    just
+    pkg-config
+  ];
+
+  buildInputs = [
+    alsa-lib
+    ffmpeg
+  ];
+
+  dontUseJustBuild = true;
+
+  justFlags = [
+    "--set"
+    "prefix"
+    (placeholder "out")
+    "--set"
+    "bin-src"
+    "target/${stdenv.hostPlatform.rust.cargoShortTarget}/release/cosmic-player"
+  ];
+
+  passthru.updateScript = nix-update-script {
+    extraArgs = [ "--version-regex" "epoch-(.*)" ];
+  };
+
+  meta = with lib; {
+    homepage = "https://github.com/pop-os/cosmic-player";
+    description = "Media player for the COSMIC Desktop Environment";
+    license = licenses.gpl3Only;
+    maintainers = with maintainers; [ /*lilyinstarlight*/ ];
+    platforms = platforms.linux;
+    mainProgram = "cosmic-player";
+  };
+}
