ecc19584d4a4b9edfec21dccd25391cdc9656fcb
Lily Foster <lily@lily.flowers>
2024-09-04 16:14:24 -0400

    cosmic-wallpapers: init at 0-unstable-2024-06-26

---
+++ pkgs/cosmic-wallpapers/package.nix
@@ -0,0 +1,28 @@
+{ lib
+, stdenvNoCC
+, fetchFromGitHub
+, nix-update-script
+}:
+
+stdenvNoCC.mkDerivation {
+  pname = "cosmic-wallpapers";
+  version = "0-unstable-2024-06-26";
+
+  src = fetchFromGitHub {
+    owner = "pop-os";
+    repo = "cosmic-wallpapers";
+    rev = "0f2f16dc39ff1281a56680e37719e98a1bc8cb99";
+    sha256 = "sha256-t98ZDmazsPxXNXI4cNDkIo61/eKQaJg9lZa4CsfX00g=";
+  };
+
+  makeFlags = [ "prefix=${placeholder "out"}" ];
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = with lib; {
+    description = "Wallpapers for the COSMIC Desktop Environment";
+    homepage = "https://github.com/pop-os/cosmic-wallpapers";
+    license = with licenses; [ cc-by-sa-40 cc-by-40 publicDomain ];
+    maintainers = with maintainers; [ /*lilyinstarlight*/ ];
+  };
+}
