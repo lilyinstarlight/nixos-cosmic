678ba77a0115441b675ff10135b8fd40060c36c4
Lily Foster <lily@lily.flowers>
2024-09-07 11:03:41 -0400

    cosmic-forecast: init at 0-unstable-2024-09-06

---
+++ pkgs/cosmic-forecast/package.nix
@@ -0,0 +1,74 @@
+{ lib
+, fetchFromGitHub
+, libcosmicAppHook
+, rustPlatform
+, just
+, openssl
+, pkg-config
+, stdenv
+, nix-update-script
+}:
+
+rustPlatform.buildRustPackage rec {
+  pname = "cosmic-forecast";
+  version = "0-unstable-2024-09-06";
+
+  src = fetchFromGitHub {
+    owner = "cosmic-utils";
+    repo = "forecast";
+    rev = "75e6cf93348c928c0f5b20e45b619f7c9a44f9c9";
+    hash = "sha256-//wVcK18b+uykoDACnUrnRdn++91Umgi877GxwcCwUQ=";
+  };
+
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+    outputHashes = {
+      "accesskit-0.12.2" = "sha256-1UwgRyUe0PQrZrpS7574oNLi13fg5HpgILtZGW6JNtQ=";
+      "atomicwrites-0.4.2" = "sha256-QZSuGPrJXh+svMeFWqAXoqZQxLq/WfIiamqvjJNVhxA=";
+      "clipboard_macos-0.1.0" = "sha256-cG5vnkiyDlQnbEfV2sPbmBYKv1hd3pjJrymfZb8ziKk=";
+      "cosmic-config-0.1.0" = "sha256-zamYPvxmIqh4IT4G+aqceP1mXNNBA1TAcJwAtjlbYAU=";
+      "cosmic-text-0.12.1" = "sha256-5Gk220HTiHuxDvyqwz1Dwr+BaLvH/6X7M14IirQzcsE=";
+      "d3d12-0.19.0" = "sha256-usrxQXWLGJDjmIdw1LBXtBvX+CchZDvE8fHC0LjvhD4=";
+      "glyphon-0.5.0" = "sha256-j1HrbEpUBqazWqNfJhpyjWuxYAxkvbXzRKeSouUoPWg=";
+      "smithay-clipboard-0.8.0" = "sha256-4InFXm0ahrqFrtNLeqIuE3yeOpxKZJZx+Bc0yQDtv34=";
+      "softbuffer-0.4.1" = "sha256-a0bUFz6O8CWRweNt/OxTvflnPYwO5nm6vsyc/WcXyNg=";
+      "taffy-0.3.11" = "sha256-SCx9GEIJjWdoNVyq+RZAGn0N71qraKZxf9ZWhvyzLaI=";
+      "winit-0.29.10" = "sha256-ScTII2AzK3SC8MVeASZ9jhVWsEaGrSQ2BnApTxgfxK4=";
+    };
+  };
+
+  nativeBuildInputs = [
+    libcosmicAppHook
+    just
+    pkg-config
+  ];
+
+  buildInputs = [
+    openssl
+  ];
+
+  dontUseJustBuild = true;
+  dontUseJustCheck = true;
+
+  justFlags = [
+    "--set"
+    "prefix"
+    (placeholder "out")
+    "--set"
+    "bin-src"
+    "target/${stdenv.hostPlatform.rust.cargoShortTarget}/release/cosmic-ext-forecast"
+  ];
+
+  env.VERGEN_GIT_SHA = src.rev;
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = with lib; {
+    homepage = "https://github.com/cosmic-utils/forecast";
+    description = "Weather forecast for the COSMIC Desktop Environment";
+    license = licenses.gpl3Only;
+    maintainers = with maintainers; [ /*lilyinstarlight*/ ];
+    platforms = platforms.linux;
+    mainProgram = "cosmic-ext-forecast";
+  };
+}
