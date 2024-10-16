0d07a92ecc7d140e52f947cc27c3107cac3744dd
atagen <atagen@users.noreply.github.com>
2024-10-17 09:20:45 +1100

    nixos/cosmic: clarify restrictions around cosmic-files exclusion
    Co-authored-by: Lily Foster <lily@lily.flowers>

--- nixos/cosmic/module.nix
+++ nixos/cosmic/module.nix
@@ -162,8 +162,12 @@ in
     # required for screen locker
     security.pam.services.cosmic-greeter = { };
 
-    warnings = if (builtins.elem pkgs.cosmic-files config.environment.cosmic.excludePackages)
-      then [ ''The COSMIC Session may fail to initialise without the cosmic-files package (excluded).'' ]
-      else [];
+    warnings = lib.optional (lib.elem pkgs.cosmic-files config.environment.cosmic.excludePackages -> !(lib.elem pkgs.cosmic-session config.environment.cosmic.excludePackages)) ''
+      The COSMIC session may fail to initialise with the `cosmic-files` package excluded via `config.environment.cosmic.excludePackages`.
+      
+      Please do one of the following:
+        1. Remove `cosmic-files` from `config.environment.cosmic.excludePackages`.
+        2. Add `cosmic-session` (in addition to `cosmic-files`) to `config.environment.cosmic.excludePackages` and ensure whatever session starter/manager you are using is appropriately set up.
+    '';
   };
 }
