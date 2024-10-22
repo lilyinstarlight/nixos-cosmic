a2e3670fc2c64513c3950a2149dfa1d2da4ff2ef
Lily Foster <lily@lily.flowers>
2024-10-22 10:46:12 -0400

    impure: fix impure entrypoint tarball URLs

--- default.nix
+++ default.nix
@@ -5,7 +5,7 @@ let
       let
         lock = builtins.fromJSON (builtins.readFile ./flake.lock);
       in fetchTarball {
-        url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.${lock.nodes.${lock.root}.inputs.flake-compat}.locked.rev}.tar.gz";
+        url = "https://github.com/nix-community/flake-compat/archive/${lock.nodes.${lock.nodes.${lock.root}.inputs.flake-compat}.locked.rev}.tar.gz";
         sha256 = lock.nodes.flake-compat.locked.narHash;
       }
     )
