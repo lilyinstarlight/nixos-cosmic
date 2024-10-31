31d0f10695628327045e58a9bb6829ba2a806647
Lily Foster <lily@lily.flowers>
2024-10-31 14:52:05 -0400

    cosmic-idle: init at 0-unstable-2024-10-30

---
+++ pkgs/cosmic-idle/package.nix
@@ -0,0 +1,66 @@
+{
+  lib,
+  fetchFromGitHub,
+  rustPlatform,
+  libcosmicAppHook,
+  just,
+  stdenv,
+  nix-update-script,
+}:
+
+rustPlatform.buildRustPackage {
+  pname = "cosmic-idle";
+  version = "0-unstable-2024-10-30";
+
+  src = fetchFromGitHub {
+    owner = "pop-os";
+    repo = "cosmic-idle";
+    rev = "08c1cf318e91831b6cbfd2c94d2fafbedb90919f";
+    hash = "sha256-ah2L8yq36U6ez+kU6C3ot0V8Imj4fw4QOW1NpjYOSYk=";
+  };
+
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+    outputHashes = {
+      "atomicwrites-0.4.2" = "sha256-QZSuGPrJXh+svMeFWqAXoqZQxLq/WfIiamqvjJNVhxA=";
+      "clipboard_macos-0.1.0" = "sha256-cG5vnkiyDlQnbEfV2sPbmBYKv1hd3pjJrymfZb8ziKk=";
+      "cosmic-config-0.1.0" = "sha256-FRpZRiT8RJuvD0OBnkDLYdd72WDZEktnAp4YgvKQ0AA=";
+      "cosmic-settings-config-0.1.0" = "sha256-QnRicNbKKAjq12hPE6QbtyH0rV33H3RPHdISYjHX7yw=";
+      "smithay-clipboard-0.8.0" = "sha256-4InFXm0ahrqFrtNLeqIuE3yeOpxKZJZx+Bc0yQDtv34=";
+      "upower_dbus-0.3.2" = "sha256-mklNPKVMO6iFrxki2DwiL5K78KiWpGxksisYldaASIE=";
+    };
+  };
+
+  nativeBuildInputs = [
+    libcosmicAppHook
+    just
+  ];
+
+  dontUseJustBuild = true;
+  dontUseJustCheck = true;
+
+  justFlags = [
+    "--set"
+    "prefix"
+    (placeholder "out")
+    "--set"
+    "bin-src"
+    "target/${stdenv.hostPlatform.rust.cargoShortTarget}/release/cosmic-idle"
+  ];
+
+  passthru.updateScript = nix-update-script {
+    # TODO: uncomment when there are actual tagged releases
+    #extraArgs = [ "--version-regex" "epoch-(.*)" ];
+  };
+
+  meta = with lib; {
+    homepage = "https://github.com/pop-os/cosmic-idle";
+    description = "Idle daemon for the COSMIC Desktop Environment";
+    license = licenses.gpl3Only;
+    maintainers = with maintainers; [
+      # lilyinstarlight
+    ];
+    platforms = platforms.linux;
+    mainProgram = "cosmic-idle";
+  };
+}
