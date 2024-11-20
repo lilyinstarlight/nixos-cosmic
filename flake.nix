56330c92af51bb4cc0823c3ce3fc0c7031322a3b
Thatoo <Thatoo@users.noreply.github.com>
2024-11-20 09:54:41 +0100

    flake,readme: bump nixpkgs-stable to 24.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
 
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
