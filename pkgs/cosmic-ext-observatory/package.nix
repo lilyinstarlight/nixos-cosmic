c1872dcd2a5264d0d52fdf4710f9523c8c077ec5
Lily Foster <lily@lily.flowers>
2024-12-12 13:33:23 -0500

    cosmic-ext-observatory: add in the nvtop dependency using suspicious methods

--- pkgs/cosmic-ext-observatory/package.nix
+++ pkgs/cosmic-ext-observatory/package.nix
@@ -3,8 +3,13 @@
   fetchFromGitHub,
   libcosmicAppHook,
   rustPlatform,
+  gnused,
+  gnutar,
+  jq,
   just,
+  mesa,
   stdenv,
+  udev,
   nix-update-script,
 }:
 
@@ -37,13 +42,41 @@ rustPlatform.buildRustPackage {
     };
   };
 
+  nvtop = fetchFromGitHub {
+    owner = "Syllo";
+    repo = "nvtop";
+    rev = "6e91c745cd051d902fc57e9195068df03eb192bb";
+    hash = "sha256-1wKNBRlWAsXcWAXrNhYg2TBTxkE0QnMig0hunHkQLFA=";
+  };
+
   nativeBuildInputs = [
     libcosmicAppHook
+    gnused
+    gnutar
+    jq
     just
   ];
 
+  buildInputs = [
+    mesa
+    udev
+  ];
+
+  postPatch = ''
+    nvtop_json="observatory-daemon/3rdparty/nvtop/nvtop.json"
+    nvtop_archive="target/${stdenv.hostPlatform.rust.cargoShortTarget}/release/build/native/$(jq -r '(."source-url" | split("/"))[-1]' "$nvtop_json")"
+    mkdir -p "$(dirname "$nvtop_archive")"
+    tar -czf "$nvtop_archive" --absolute-names --transform="s,$nvtop,$(jq -r '.directory' "$nvtop_json")," --mode=+w "$nvtop"
+    sed -i -e 's/\("source-hash":\s*"\)[^"]*\("\)/\1'"$(sha256sum -b "$nvtop_archive" | cut -d' ' -f1)"'\2/' "$nvtop_json"
+
+    substituteInPlace observatory-daemon/build/build.rs \
+      --replace-fail "$(printf '    #[cfg(not(debug_assertions))]\n    build_def.flag("-flto");\n')" ""
+  '';
+
   dontUseJustBuild = true;
   dontUseJustCheck = true;
+
+  doCheck = false;
 
   justFlags = [
     "--set"
@@ -52,7 +85,14 @@ rustPlatform.buildRustPackage {
     "--set"
     "bin-src"
     "target/${stdenv.hostPlatform.rust.cargoShortTarget}/release/observatory"
+    "--set"
+    "dae-src"
+    "target/${stdenv.hostPlatform.rust.cargoShortTarget}/release/observatory-daemon"
   ];
+
+  postInstall = ''
+    libcosmicAppWrapperArgs+=(--prefix PATH : $out/bin)
+  '';
 
   passthru.updateScript = nix-update-script { };
 
