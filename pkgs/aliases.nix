c0027e303d02fcb8d51285e9a529c81f37e56382
Heitor Augusto <IAm.HeitorALN@proton.me>
2025-01-24 16:15:21 -0300

    pkgs: rename `cosmic-ext-observatory` to `observatory`

--- pkgs/aliases.nix
+++ pkgs/aliases.nix
@@ -2,5 +2,6 @@ pkgs:
 
 {
   cosmic-ext-examine = pkgs.lib.warn "`cosmic-ext-examine` has been renamed to `examine`." pkgs.examine;
+  cosmic-ext-observatory = pkgs.lib.warn "`cosmic-ext-observatory` has been renamed to `observatory`." pkgs.observatory;
   cosmic-ext-tasks = pkgs.lib.warn "`cosmic-ext-tasks` has been renamed to `tasks`." pkgs.tasks;
 }
