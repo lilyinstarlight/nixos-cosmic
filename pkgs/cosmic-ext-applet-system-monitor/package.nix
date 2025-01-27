3960559bb55a90ea8aa8bd5634992d77a12a4d9e
ziguana <git@ziguana.dev>
2025-01-27 11:56:37 -0700

    copy clipboard manager

---
+++ pkgs/cosmic-ext-applet-system-monitor/package.nix
@@ -0,0 +1,61 @@
+{
+  lib,
+  fetchFromGitHub,
+  libcosmicAppHook,
+  rustPlatform,
+  just,
+  stdenv,
+  nix-update-script,
+}:
+
+rustPlatform.buildRustPackage {
+  pname = "cosmic-ext-applet-clipboard-manager";
+  version = "0.1.0-unstable-2025-01-18";
+
+  src = fetchFromGitHub {
+    owner = "cosmic-utils";
+    repo = "clipboard-manager";
+    rev = "b347d7dcea3fb983af8d0d499d1721c739a723f3";
+    hash = "sha256-K4m5q0/4IwD/TYtn/aCKFCHMz1nKH/gV+DCL4y9WY+I=";
+  };
+
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-OabpLa0mkpSOXIiJnNbnbV2QU8aTC6ybdwLNBpjm0aQ=";
+
+  nativeBuildInputs = [
+    libcosmicAppHook
+    just
+  ];
+
+  dontUseJustBuild = true;
+  dontUseJustCheck = true;
+
+  justFlags = [
+    "--set"
+    "prefix"
+    (placeholder "out")
+    "--set"
+    "env-dst"
+    "${placeholder "out"}/etc/profile.d/cosmic-ext-applet-clipboard-manager.sh"
+    "--set"
+    "bin-src"
+    "target/${stdenv.hostPlatform.rust.cargoShortTarget}/release/cosmic-ext-applet-clipboard-manager"
+  ];
+
+  preCheck = ''
+    export XDG_RUNTIME_DIR="$TMP"
+  '';
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    homepage = "https://github.com/cosmic-utils/clipboard-manager";
+    description = "Clipboard manager for the COSMIC Desktop Environment";
+    license = lib.licenses.gpl3Only;
+    maintainers = with lib.maintainers; [
+      # lilyinstarlight
+    ];
+    platforms = lib.platforms.linux;
+    mainProgram = "cosmic-ext-applet-clipboard-manager";
+  };
+}
