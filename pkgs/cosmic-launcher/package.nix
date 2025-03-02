1b83443f489ce2c9ecee2751cc269e366d8a7a1e
Heitor Augusto <IAm.HeitorALN@proton.me>
2025-03-02 18:56:51 -0300

    cosmic-launcher: use mold linker

--- pkgs/cosmic-launcher/package.nix
+++ pkgs/cosmic-launcher/package.nix
@@ -1,61 +1,67 @@
 {
   lib,
+  stdenv,
+  stdenvAdapters,
   fetchFromGitHub,
   rustPlatform,
   libcosmicAppHook,
   just,
-  stdenv,
   nix-update-script,
+
+  withMoldLinker ? stdenv.targetPlatform.isLinux,
 }:
 
-rustPlatform.buildRustPackage {
-  pname = "cosmic-launcher";
-  version = "1.0.0-alpha.6-unstable-2025-02-19";
+rustPlatform.buildRustPackage.override
+  { stdenv = if withMoldLinker then stdenvAdapters.useMoldLinker stdenv else stdenv; }
+  {
+    pname = "cosmic-launcher";
+    version = "1.0.0-alpha.6-unstable-2025-02-19";
 
-  src = fetchFromGitHub {
-    owner = "pop-os";
-    repo = "cosmic-launcher";
-    rev = "b617a9dc68b2365e65b7820a6c81980159956a1c";
-    hash = "sha256-BtYnL+qkM/aw+Air5yOKH098V+TQByM5mh1DX7v+v+s=";
-  };
+    src = fetchFromGitHub {
+      owner = "pop-os";
+      repo = "cosmic-launcher";
+      rev = "b617a9dc68b2365e65b7820a6c81980159956a1c";
+      hash = "sha256-BtYnL+qkM/aw+Air5yOKH098V+TQByM5mh1DX7v+v+s=";
+    };
 
-  useFetchCargoVendor = true;
-  cargoHash = "sha256-g7Qr3C8jQg65KehXAhftdXCpEukag0w12ClvZFkxfqs=";
+    useFetchCargoVendor = true;
+    cargoHash = "sha256-g7Qr3C8jQg65KehXAhftdXCpEukag0w12ClvZFkxfqs=";
 
-  nativeBuildInputs = [
-    libcosmicAppHook
-    just
-  ];
-
-  dontUseJustBuild = true;
-  dontUseJustCheck = true;
-
-  justFlags = [
-    "--set"
-    "prefix"
-    (placeholder "out")
-    "--set"
-    "bin-src"
-    "target/${stdenv.hostPlatform.rust.cargoShortTarget}/release/cosmic-launcher"
-  ];
-
-  env."CARGO_TARGET_${stdenv.hostPlatform.rust.cargoEnvVarTarget}_RUSTFLAGS" = "--cfg tokio_unstable";
-
-  passthru.updateScript = nix-update-script {
-    extraArgs = [
-      "--version-regex"
-      "epoch-(.*)"
+    nativeBuildInputs = [
+      libcosmicAppHook
+      just
     ];
-  };
 
-  meta = {
-    homepage = "https://github.com/pop-os/cosmic-launcher";
-    description = "Launcher for the COSMIC Desktop Environment";
-    license = lib.licenses.gpl3Only;
-    maintainers = with lib.maintainers; [
-      # lilyinstarlight
+    dontUseJustBuild = true;
+    dontUseJustCheck = true;
+
+    justFlags = [
+      "--set"
+      "prefix"
+      (placeholder "out")
+      "--set"
+      "bin-src"
+      "target/${stdenv.hostPlatform.rust.cargoShortTarget}/release/cosmic-launcher"
     ];
-    platforms = lib.platforms.linux;
-    mainProgram = "cosmic-launcher";
-  };
-}
+
+    env."CARGO_TARGET_${stdenv.hostPlatform.rust.cargoEnvVarTarget}_RUSTFLAGS" =
+      "--cfg tokio_unstable${lib.optionalString withMoldLinker " -C link-arg=-fuse-ld=mold"}";
+
+    passthru.updateScript = nix-update-script {
+      extraArgs = [
+        "--version-regex"
+        "epoch-(.*)"
+      ];
+    };
+
+    meta = {
+      homepage = "https://github.com/pop-os/cosmic-launcher";
+      description = "Launcher for the COSMIC Desktop Environment";
+      license = lib.licenses.gpl3Only;
+      maintainers = with lib.maintainers; [
+        # lilyinstarlight
+      ];
+      platforms = lib.platforms.linux;
+      mainProgram = "cosmic-launcher";
+    };
+  }
